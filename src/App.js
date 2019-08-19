86d79f9a6b96720fbbd1188371ffa5b1cf754e91
David Quiñones <daquinons@users.noreply.github.com>
2019-08-19 15:25:36 +0200

    Move Router to App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Index from './components/pages/Index';
 import Login from './components/pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/login/">Login</Link>
-          </li>
-        </ul>
-      </nav>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login/">Login</Link>
+            </li>
+          </ul>
+        </nav>
 
-      <Route path="/" exact component={Index} />
-      <Route path="/login/" component={Login} />
-    </div>
+        <Route path="/" exact component={Index} />
+        <Route path="/login/" component={Login} />
+      </div>
+    </Router>
   );
 }
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-<<<<<<< HEAD
 import * as serviceWorker from './utils/serviceWorker';
-=======
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
->>>>>>> Add Route for Index and Login
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
