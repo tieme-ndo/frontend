9c86c53d45f25a7cc83318c0af997da8758c3b1f
David Quiñones <daquinons@users.noreply.github.com>
2019-08-19 11:43:03 +0200

    Add Route for Index and Login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import Index from './components/pages/Index';
+import Login from './components/pages/Login';
 
 function App() {
   return (
-    <div className="App" data-testid="App">
-      <header>
-        <h1>Tieme Ndo</h1>
-      </header>
+    <div className="App">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login/">Login</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Route path="/" exact component={Index} />
+      <Route path="/login/" component={Login} />
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/pages/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <header>
+      <h1>Tieme Ndo Login</h1>
+    </header>
+  );
+};
+
+export default Login;
