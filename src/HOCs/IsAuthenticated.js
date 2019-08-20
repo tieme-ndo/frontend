71f380a14fb222eb5fdf5fb1076321ece44d649c
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-08-20 16:38:11 +0200

    Removing request handling. Leaving authentication testing to 'Blocked' for now. Moving to separate branch for request handling

--- src/HOCs/IsAuthenticated.js
+++ src/HOCs/IsAuthenticated.js
@@ -1,21 +1,25 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
+// const jwt = require('jsonwebtoken');
 
 const IsAuthenticated = WrappedComponent => {
 
   const HOCComponent = props => {
-    let tokenInPlace = localStorage.getItem('token')
-    // if (!tokenInPlace) {
-    //   return <Redirect to="/" />;
-    // } else {
-    //   return <WrappedComponent {...props} />;
-    // }
-    if (!tokenInPlace) {
-      return null
-    } else {
-      return <WrappedComponent {...props} />
-    }
-  };
+    // Retrieve token from Local Storage
+    let token = localStorage.getItem('token')
+    
+      if (token) {
+        // jwt.verify(token, PROCESS.ENV.SECRET, (err, decodedToken) => {
+          if (err) {
+            // Redirect
+          } else {
+            // Return Wrapped Component
+          }
+        // });
+      } else {
+        // Redirect
+      }
+  }
 };
 
 export default IsAuthenticated;
