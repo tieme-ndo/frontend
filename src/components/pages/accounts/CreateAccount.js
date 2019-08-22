97b1e02f37ab11368bc554c024e7ea2606ef6f0c
David Quiñones <daquinons@users.noreply.github.com>
2019-08-22 15:52:54 +0200

    Add event handler and basic message display

--- src/components/pages/accounts/CreateAccount.js
+++ src/components/pages/accounts/CreateAccount.js
@@ -5,23 +5,59 @@ const CreateAccount = () => {
   const passwordRef = React.useRef();
   const isAdminRef = React.useRef();
 
+  const [errorMessage, setErrorMessage] = React.useState(undefined);
+  const [message, setMessage] = React.useState(undefined);
+  const [loading, setLoading] = React.useState(false);
+
+  const onCreate = event => {
+    event.preventDefault();
+    setLoading(true);
+    // To make request using client
+    // if success, setMessage(message)
+    // if error, setErrorMessage(error)
+    // setLoading(false);
+  };
+
   return (
     <div>
       <h2>Create New Account</h2>
-      <form>
+      {errorMessage && <div className="error-message">{errorMessage}</div>}
+      {message && <div className="message">{message}</div>}
+      <form onSubmit={onCreate}>
         <div>
           <label htmlFor="username">Username</label>
-          <input ref={usernameRef} id="username" name="username" type="text" />
+          <input
+            ref={usernameRef}
+            id="username"
+            name="username"
+            type="text"
+          />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input ref={passwordRef} id="password" name="password" type="password" />
+          <input
+            ref={passwordRef}
+            id="password"
+            name="password"
+            type="password"
+          />
         </div>
         <div>
           <label htmlFor="is-admin">Is Admin?</label>
-          <input ref={isAdminRef} type="checkbox" id="is-admin" name="is-admin" />
+          <input
+            ref={isAdminRef}
+            type="checkbox"
+            id="is-admin"
+            name="is-admin"
+          />
         </div>
-        <button type="submit">Create Account</button>
+        {loading ? (
+          <button type="submit" disabled>
+            Loading...
+          </button>
+        ) : (
+          <button type="submit">Create Account</button>
+        )}
       </form>
     </div>
   );
