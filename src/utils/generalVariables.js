82c3ea84810cfd40f7466c8468a30a03a2266a68
David Quiñones <daquinons@users.noreply.github.com>
2019-08-23 14:24:22 +0200

    Add real API endpoints

--- src/utils/generalVariables.js
+++ src/utils/generalVariables.js
@@ -6,13 +6,11 @@ const URL = 'https://tieme-ndo-backend-staging1.herokuapp.com/api/v1';
 // Path contains the 'base' of the URL but does not includes params
 // These will have to be included separately in the Axios request
 export const pathObj = {
+  getUserPath: `${URL}/user`,
   loginPath: `${URL}/user/login`,
-  registrationPath: `${URL}/register`,
-
-  getUserPath: `${URL}/users`,
-  changePasswordPath: `${URL}/users/change-password`,
-  registrationPath: `${URL}/users/signup`,
-  deleteUserPath: `${URL}/users`,
+  changePasswordPath: `${URL}/user/change-password`,
+  registrationPath: `${URL}/user/signup`,
+  deleteUserPath: `${URL}/user`,
 
   getFarmersPath: `${URL}/farmers`,
   addFarmerPath: `${URL}/farmers`,
