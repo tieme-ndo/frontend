0364cc21a6d88bd584e905e283c6ed83474befed
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-08-27 13:58:42 +0200

    Testing for environmental variables on Netlify

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './components/pages/Login';
 import Dashboard from './components/pages/Dashboard';
 
 function App() {
+  console.log(process.env)
   return (
     <Router>
       <div className="App" data-testid="App">
