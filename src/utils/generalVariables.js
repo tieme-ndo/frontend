2f928b39cea8764e2e7372152de7b8e0aa9c3594
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-08-27 14:18:42 +0200

    Changing variable names and disabling service workers for the moment

--- src/utils/generalVariables.js
+++ src/utils/generalVariables.js
@@ -1,7 +1,7 @@
 // Retrieving URL from environment - compatible with deployment context
 // When running on a local machine, the URL will default to REACT_APP_API_URL - provided the app is run using `yarn start`
 // When hosted on Netlify, it'll retrieve the requisite environmental URL and target the appropriate API
-export const URL = process.env.API_URL || process.env.REACT_APP_API_URL;
+export const URL = process.env.REACT_APP_API_URL;
 
 // Path contains the 'base' of the URL but does not includes params
 // These will have to be included separately in the Axios request
