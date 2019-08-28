32c36b3b338d0214b91ff6d837ffd6deb932613a
abidex4yemi <abidex4yemi@gmail.com>
2019-08-28 08:53:26 +0100

    ch(design): add custom design theme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'semantic-ui-less/semantic.less';
 import * as serviceWorker from './utils/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
