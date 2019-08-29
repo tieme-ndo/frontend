b2855963e032a4fd7578f54ac6f10dbcd0c88cf3
Pav0l <pavol.karas@gmail.com>
2019-08-29 21:36:23 +0200

    Update redirect to dashboard

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -3,13 +3,14 @@ import { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import {
   loginHandler,
-//   getToken
+  getUser,
+  isLoggedIn
 } from '../../../utils/handlers/authenticationHandlers';
 import PropTypes from 'prop-types';
 import LoginForm from './LoginForm';
 import validateLoginForm from './loginValidation';
 
-function Login({ setUser }) {
+function Login({ setUser, ...props}) {
   const [state, setState] = useState({
     username: '',
     password: '',
@@ -17,26 +18,6 @@ function Login({ setUser }) {
     loginIn: false
   });
 
-//   const [status, changeStatus] = useState({
-//     loading: false,
-//     error: false
-//   });
-
-//   const submitLogin = async event => {
-//     event.preventDefault();
-//     changeStatus({ ...status, loading: true });
-//     try {
-//       const receivedUser = await loginHandler({
-//         username: credentials.username,
-//         password: credentials.password
-//       });
-//       changeStatus({ error: false, loading: false });
-//       setUser(receivedUser);
-//       return <Redirect to="/" />;
-//     } catch (err) {
-//       changeStatus({ error: true, loading: false });
-//     }
-//   };
 
   const handleChange = (e, { name, value }) => {
     setState(prevState => ({ ...prevState, [name]: value }));
@@ -72,13 +53,9 @@ function Login({ setUser }) {
 
     // if the response has token => successful login
     if (receivedUser.hasOwnProperty('token')) {
-        setUser(receivedUser);
-        setState(prevState => ({
-            ...prevState,
-            loginIn: false
-          }));
+        setUser(getUser());
 
-        return <Redirect to="/" />;
+        props.history.push('/');
     } else {
         setState(prevState => ({
             ...prevState,
@@ -86,14 +63,11 @@ function Login({ setUser }) {
             loginIn: false
         }))
     }
-
 }
 
-
-
-//   if (getToken()) {
-//     return <Redirect to="/" />;
-//   }
+  if (isLoggedIn()) {
+    return <Redirect to="/" />;
+  }
 
   return (
     <LoginForm
@@ -101,38 +75,6 @@ function Login({ setUser }) {
     handleSubmit={handleSubmit}
     handleChange={handleChange}
   />
-    // <div>
-    //   <div>
-    //     <h1>Log In</h1>
-    //     <form onSubmit={submitLogin}>
-    //       <input
-    //         className="ui field"
-    //         type="username"
-    //         placeholder="Enter Username"
-    //         value={credentials.username}
-    //         onChange={e =>
-    //           changeCredentials({ ...credentials, username: e.target.value })
-    //         }
-    //       />
-    //       <input
-    //         type="password"
-    //         placeholder="Enter Password"
-    //         value={credentials.password}
-    //         onChange={e =>
-    //           changeCredentials({ ...credentials, password: e.target.value })
-    //         }
-    //       />
-    //       {status.loading ? (
-    //         <input type="submit" value="Loading..." disabled />
-    //       ) : (
-    //         <Button content="Primary" primary />
-    //       )}
-    //       {status.error && (
-    //         <div>Wrong username or password, please try again</div>
-    //       )}
-    //     </form>
-    //   </div>
-    // </div>
   );
 }
 
