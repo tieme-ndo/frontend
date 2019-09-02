d4679460b769952887f12c259336560ef04297da
abidex4yemi <abidex4yemi@gmail.com>
2019-09-02 12:26:06 +0100

    delete: prttier config file

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,3 @@
-/** @format */
-
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import withRestrictedAccess from '../hoc/withRestrictedAccess';
@@ -7,7 +5,6 @@ import Dashboard from '../pages/Dashboard/Dashboard';
 import Login from '../pages/Login/Login';
 import CreateAccount from '../pages/CreateAccount/CreateAccount';
 import { getUser } from '../../utils/handlers/authenticationHandlers';
-import { logout } from '../../utils/handlers/authenticationHandlers';
 import AddFarmer from '../pages/AddFarmer/AddFarmer';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -22,10 +19,6 @@ function App() {
     }
   }, [user]);
 
-  const logOut = () => {
-    setUser(false);
-    logout();
-  };
   return (
     <Router>
       <div className="App" data-testid="App">
