8603b33d143d115e540e33fef96e375e2d89633e
abidex4yemi <abidex4yemi@gmail.com>
2019-09-02 13:05:56 +0100

    Complete: user dashboard navigation

--- src/components/common/PageHeader/PageHeader.js
+++ src/components/common/PageHeader/PageHeader.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logo from '../../../assets/images/tiemendo_logo.jpg';
 import { Container, Dropdown, Image, Menu } from 'semantic-ui-react';
 
 const PageHeader = () => {
   return (
-    <Menu>
+    <Menu style={{ borderRadius: '0', margin: '0' }}>
       <Container>
         <Menu.Item>
           <Image style={{ width: '55px' }} src={logo} alt="tiemendo logo" />
@@ -14,7 +15,9 @@ const PageHeader = () => {
         <Menu.Menu position="right">
           <Dropdown text="settings" pointing className="link item">
             <Dropdown.Menu>
-              <Dropdown.Item> Create new account</Dropdown.Item>
+              <Dropdown.Item>
+                <Link to="/accounts/new">Create new account</Link>
+              </Dropdown.Item>
               <Dropdown.Item>Logout</Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
