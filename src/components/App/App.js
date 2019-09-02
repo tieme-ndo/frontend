8820eb174694061588e3d5df476ccadfdb7dbc61
Rexben <rexben.rb@gmail.com>
2019-09-02 11:34:34 +0100

    replace Router with HashRouter

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import React, {useState, useEffect} from 'react'
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import {HashRouter as Router, Route, Link} from 'react-router-dom'
 import withRestrictedAccess from '../hoc/withRestrictedAccess'
 import Dashboard from '../pages/Dashboard/Dashboard'
 import Login from '../pages/Login/Login'
