bb2a2ccf579afa2b6d06df2149c8a5f56c487cc3
Pav0l <pavol.karas@gmail.com>
2019-09-03 17:38:10 +0200

    Rename dropdown, add link to logo

--- src/components/common/PageHeader/PageHeader.js
+++ src/components/common/PageHeader/PageHeader.js
@@ -1,19 +1,21 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import logo from "../../../assets/images/tiemendo_logo.jpg";
-import { Container, Dropdown, Image, Menu } from "semantic-ui-react";
+import logo from '../../../assets/images/tiemendo_logo.jpg';
+import { Container, Dropdown, Image, Menu } from 'semantic-ui-react';
 
 const PageHeader = ({ logOut, user }) => {
   return (
-    <Menu style={{ borderRadius: "0", margin: "0" }}>
+    <Menu style={{ borderRadius: '0', margin: '0' }}>
       <Container>
-        <Menu.Item>
-          <Image style={{ width: "55px" }} src={logo} alt="tiemendo logo" />
-        </Menu.Item>
+        <Link to="/">
+          <Menu.Item>
+            <Image style={{ width: '55px' }} src={logo} alt="tiemendo logo" />
+          </Menu.Item>
+        </Link>
 
         <Menu.Menu position="right">
-          <Dropdown text="settings" pointing className="link item">
+          <Dropdown text="Menu" pointing className="link item">
             <Dropdown.Menu>
               <Link to="/">
                 <Dropdown.Item>Dashboard</Dropdown.Item>
