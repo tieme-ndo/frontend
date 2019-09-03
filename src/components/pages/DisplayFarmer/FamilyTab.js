af473004422e3838e7461cb00eec04201decc432
Shola Ayeni <ayenisholah@yahoo.com>
2019-09-03 12:36:32 +0100

    created family tab component

--- src/components/pages/DisplayFarmer/FamilyTab.js
+++ src/components/pages/DisplayFarmer/FamilyTab.js
@@ -1,39 +1,31 @@
 import React from 'react';
 import { Grid, Header } from 'semantic-ui-react';
 
-export default function DisplayTabTemplate(props) { //Props will contain the farmer object
-
+export default function FamilyTab(props) {
   const data = props.farmer.familyInfo;
   console.log(data);
 
   return (
     <Grid columns={2}>
-      <Grid.Column >
-        <Header.Subheader>Grey Title</Header.Subheader>
+      <Grid.Column>
+        <Header.Subheader>Family Income Per Month</Header.Subheader>
         <Header size="tiny" style={{ marginTop: '0.1rem' }}>
-          Farmer Content
+          {data.family_income_per_month}
         </Header>
-        <Header.Subheader> Grey Title</Header.Subheader>
+        <Header.Subheader>Family Size</Header.Subheader>
         <Header size="tiny" style={{ marginTop: '0.1rem' }}>
-          Farmer Content
+          {data.family_size}
         </Header>
-        <Header.Subheader> Grey Title</Header.Subheader>
+        <Header.Subheader>Number of Dependant</Header.Subheader>
         <Header size="tiny" style={{ marginTop: '0.1rem' }}>
-          Farmer Content
+          {data.number_of_dependant}
         </Header>
-      </Grid.Column>
-      <Grid.Column >
-        <Header.Subheader> Grey Title</Header.Subheader>
-        <Header size="tiny" style={{ marginTop: '0.1rem' }}>
-          Farmer Contnet
-        </Header>
-        <Header.Subheader> Grey Title</Header.Subheader>
-        <Header size="tiny" style={{ marginTop: '0.1rem' }}>
-          Farmer Content
-        </Header>
-        <Header.Subheader> Grey Title</Header.Subheader>
-        <Header size="tiny" style={{ marginTop: '0.1rem' }}>
-          Farmer Content
+        <Header.Subheader>Highest Level of Education of Dependants</Header.Subheader>
+        <Header
+          size="tiny"
+          style={{ marginTop: '0.1rem', marginBottom: '0rem' }}
+        >
+          {data.highest_level_of_dependent}
         </Header>
       </Grid.Column>
     </Grid>
