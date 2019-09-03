6fa555b8db049ce32c4bea95b1a5192bd020a246
Shola Ayeni <ayenisholah@yahoo.com>
2019-09-03 11:33:30 +0100

    created family tab

--- src/components/pages/DisplayFarmer/FamilyTab.js
+++ src/components/pages/DisplayFarmer/FamilyTab.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Grid, Header } from 'semantic-ui-react';
 
 export default function DisplayTabTemplate(props) { //Props will contain the farmer object
-  console.log(props)
 
-  const data = props.farmer.personalInfo;
+  const data = props.farmer.familyInfo;
+  console.log(data);
 
   return (
     <Grid columns={2}>
