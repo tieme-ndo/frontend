80049f788ceb9b7fab27af07d7ccddec058e27fd
David Quiñones <daquinons@users.noreply.github.com>
2019-09-03 18:27:49 +0200

    Adjust look for loading indicator

--- src/components/pages/Dashboard/LoadingIndicator.js
+++ src/components/pages/Dashboard/LoadingIndicator.js
@@ -2,24 +2,46 @@ import React from 'react';
 import { Placeholder } from 'semantic-ui-react';
 
 const LoadingIndicator = () => (
-  <Placeholder fluid>
-    <Placeholder.Line length="full" />
-    <Placeholder.Line length="full" />
-    <Placeholder.Line length="full" />
-    <Placeholder.Line length="full" />
-    <Placeholder.Line length="full" />
-    <Placeholder.Line length="full" />
-    <Placeholder.Line length="full" />
-    <Placeholder.Line length="full" />
-    <Placeholder.Line length="full" />
-    <Placeholder.Line length="full" />
-    <Placeholder.Line length="full" />
-    <Placeholder.Line length="full" />
-    <Placeholder.Line length="full" />
-    <Placeholder.Line length="full" />
-    <Placeholder.Line length="full" />
+  <Placeholder fluid style={{ backgroundColor: 'none' }}>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <div style={{ marginBottom: '1.8rem', width: '100%' }}>
+        <Placeholder.Line />
+      </div>
+      <div style={{ marginBottom: '1.8rem', width: '100%' }}>
+        <Placeholder.Line />
+      </div>
+      <div style={{ marginBottom: '1.8rem', width: '100%' }}>
+        <Placeholder.Line />
+      </div>
+      <div style={{ marginBottom: '1.8rem', width: '100%' }}>
+        <Placeholder.Line />
+      </div>
+      <div style={{ marginBottom: '1.8rem', width: '100%' }}>
+        <Placeholder.Line />
+      </div>
+      <div style={{ marginBottom: '1.8rem', width: '100%' }}>
+        <Placeholder.Line />
+      </div>
+      <div style={{ marginBottom: '1.8rem', width: '100%' }}>
+        <Placeholder.Line />
+      </div>
+      <div style={{ marginBottom: '1.8rem', width: '100%' }}>
+        <Placeholder.Line />
+      </div>
+      <div style={{ marginBottom: '1.8rem', width: '100%' }}>
+        <Placeholder.Line />
+      </div>
+      <div style={{ marginBottom: '1.8rem', width: '100%' }}>
+        <Placeholder.Line />
+      </div>
+      <div style={{ marginBottom: '1.8rem', width: '100%' }}>
+        <Placeholder.Line />
+      </div>
+      <div style={{ marginBottom: '1.8rem', width: '100%' }}>
+        <Placeholder.Line />
+      </div>
+    </div>
   </Placeholder>
 );
-
 
 export default LoadingIndicator;
