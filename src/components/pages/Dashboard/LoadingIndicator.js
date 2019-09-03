9a18fe0cd55490e280a4f1cf06b3ac479a8c0471
David Quiñones <daquinons@users.noreply.github.com>
2019-09-03 16:43:57 +0200

    Create Loading Indicator

---
+++ src/components/pages/Dashboard/LoadingIndicator.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Placeholder } from 'semantic-ui-react';
+
+const LoadingIndicator = () => (
+  <Placeholder fluid>
+    <Placeholder.Header style={{ height: '3rem' }} />
+    <Placeholder.Line length="full" />
+    <Placeholder.Line length="full" />
+    <Placeholder.Line length="full" />
+    <Placeholder.Line length="full" />
+    <Placeholder.Line length="full" />
+    <Placeholder.Line length="full" />
+    <Placeholder.Line length="full" />
+    <Placeholder.Line length="full" />
+    <Placeholder.Line length="full" />
+    <Placeholder.Line length="full" />
+    <Placeholder.Line length="full" />
+    <Placeholder.Line length="full" />
+    <Placeholder.Line length="full" />
+    <Placeholder.Line length="full" />
+    <Placeholder.Line length="full" />
+    <Placeholder.Line length="full" />
+    <Placeholder.Line length="full" />
+    <Placeholder.Line length="full" />
+    <Placeholder.Line length="full" />
+    <Placeholder.Line length="full" />
+    <Placeholder.Line length="full" />
+  </Placeholder>
+);
+
+
+export default LoadingIndicator;
