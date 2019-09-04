38045aee0c8242a4eaa956c5c180b4ddc398161c
quansenB <inaki.breinbauer@gmail.com>
2019-09-04 15:25:16 +0200

    add styling to page, rename varabiles wherever necessary, add params to make semantic Ui work with the logic, import necessary components, delete stale imports

--- src/components/pages/UpdateFarmer/UpdateFarmer.js
+++ src/components/pages/UpdateFarmer/UpdateFarmer.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import Input from '../../common/Input/Input';
 import * as form from '../../common/Input/addFarmerData';
-import Button from '../../common/Button/StyledButton';
-import styled from 'styled-components';
 import { updateFarmerHandler } from '../../../utils/handlers/farmerHandlers';
 import { getToken } from '../../../utils/handlers/authenticationHandlers';
 import withRestrictedAccess from '../../hoc/withRestrictedAccess';
-
+import { Menu, Segment, Form, Button } from 'semantic-ui-react';
+import axios from 'axios'
 const UpdateFarmer = ({ location, history, appStateShouldUpdate }) => {
 
   // Prevents errors when location state is empty
@@ -18,9 +17,11 @@ const UpdateFarmer = ({ location, history, appStateShouldUpdate }) => {
     // Deep copy of form input data objects
     const formInputData = JSON.parse(JSON.stringify(form));
 
+    // eslint-disable-next-line no-unused-vars
     for (const inputSection in formInputData) {
       const inputSectionData = formInputData[inputSection];
 
+      // eslint-disable-next-line no-unused-vars
       for (const input in inputSectionData) {
         inputSectionData[input].value = farmerData[inputSection][input];
         if ('selected' in inputSectionData[input]) {
@@ -44,35 +45,80 @@ const UpdateFarmer = ({ location, history, appStateShouldUpdate }) => {
     farmInfoToggle: true,
   });
 
-  const onChangeHandler = (e, data) => {
-    const { name, value, type } = e.target
-    const newData = { ...formElementsState[data] }
-    const newEntry = { ...newData[name] }
+  const onChangeHandler = async (e, data, elementType, elementConfigObj) => {
+    let name, value, type, files;
+
+    if (elementType === 'checkbox') {
+      // This is for the checkboxes to work as Semantic UI uses the :before pseudoelement
+      // which causes the event target to be the checkbox's label instead of the checkbox element
+      name = e.target.previousElementSibling.name;
+      value = e.target.previousElementSibling.value;
+      type = e.target.previousElementSibling.type;
+    } else if (elementType === 'select') {
+      // This is for the dropdowns to work as Semantic UI uses divs
+      // which do not have appropriate name, value, and type properties
+      name = elementConfigObj.name;
+      value = e.target.textContent;
+      type = elementConfigObj.elementType;
+    } else {
+      // For all other input types
+      name = e.target.name;
+      value = e.target.value;
+      type = e.target.type;
+
+      if (e.target.files) {
+        files = e.target.files;
+      }
+    }
+
+    const newData = { ...formElementsState[data] };
+    const newEntry = { ...newData[name] };
     if (type === 'checkbox') {
       if (newEntry.selected.indexOf(value) > -1) {
-        newEntry.selected = newEntry.selected.filter(s => s !== value)
+        newEntry.selected = newEntry.selected.filter(s => s !== value);
       } else {
-        newEntry.selected = [...newEntry.selected, value]
+        newEntry.selected = [...newEntry.selected, value];
       }
+    } else if (type === 'file') {
+      const imageFile = new FormData();
+      imageFile.append('file', files[0]);
+      imageFile.append(
+        'upload_preset',
+        process.env.REACT_APP_CLOUDINARY_PRESET
+      );
+      const imageUrl = await axios
+        .post(process.env.REACT_APP_CLOUDINARY_URL, imageFile)
+        .then(data => data.data.secure_url)
+        .catch(err => err);
+      newEntry.imageUrl = imageUrl;
     } else {
-      newEntry.value = value
+      newEntry.value = value;
     }
-    newData[name] = newEntry
+    newData[name] = newEntry;
     setFormElementsState({ ...formElementsState, [data]: newData })
   }
+
   const toggleHandler = data => {
-    setStateToggle(prevState => ({
-      ...prevState,
-      [data]: !prevState[data],
-    }))
-  }
+    setStateToggle({
+      personalInfoToggle: true,
+      familyInfoToggle: true,
+      guarantorToggle: true,
+      farmInfoToggle: true,
+      [data]: false
+    });
+
+    //scrolls to the height of the Menu whenever the tab is changed.
+    window.scrollTo(0, 75);
+  };
+
   const formHandler = e => {
     e.preventDefault()
     let formData = {}
-
     const newState = JSON.parse(JSON.stringify(formElementsState));
+    // eslint-disable-next-line no-unused-vars
     for (let key in newState) {
       formData[key] = newState[key]
+      // eslint-disable-next-line no-unused-vars
       for (let key2 in newState[key]) {
         if (newState[key][key2].selected) {
           formData[key][key2] = newState[key][key2].selected
@@ -93,20 +139,22 @@ const UpdateFarmer = ({ location, history, appStateShouldUpdate }) => {
       });
     });
   }
-  const inputCreator = (data, index) => {
+  const inputCreator = (data, tabName) => {
     const formElementsArray = []
+    // eslint-disable-next-line no-unused-vars
     for (let key in data) {
       formElementsArray.push({
         id: key,
         config: data[key],
       })
     }
-    let form = formElementsArray.map(formElement => (
+    let form = formElementsArray.map((formElement, idx) => (
       <Input
-        key={formElement.config.name}
-        {...formElement.config}
-        data={index}
-        changeHandler={onChangeHandler}
+      key={idx}
+      {...formElement.config}
+      elementConfigObj={formElement.config}
+      data={tabName}
+      changeHandler={onChangeHandler}
       />
     ))
     return form
@@ -117,68 +165,158 @@ const UpdateFarmer = ({ location, history, appStateShouldUpdate }) => {
   let guarantorInputs = inputCreator(formElementsState.guarantor, 'guarantor')
   let farmInfoInputs = inputCreator(formElementsState.farmInfo, 'farmInfo')
 
-  const DivToggle = styled.div`
-    display: flex;
-    justify-content: space-between;
-    &:hover {
-      cursor: pointer;
-    }
-  `
   return (
     <div>
-      <header>
-        <div>Logo</div>
-        <div>
-          <Button displayName="LogOut" />
-        </div>
-      </header>
-      <section>
-        <Button displayName="Back" styles={{ backgroundColor: 'green' }} />
-        <hr />
+      <Segment>
+        <Menu stackable widths="4">
+          <Menu.Item
+            name="Personal"
+            active={stateToggle.personalInfoToggle === false}
+            onClick={() => toggleHandler('personalInfoToggle')}
+          >
+            <b>1. Personal</b>
+          </Menu.Item>
+          <Menu.Item
+            name="Family"
+            active={stateToggle.familyInfoToggle === false}
+            onClick={() => toggleHandler('familyInfoToggle')}
+          >
+            <b>2. Family</b>
+          </Menu.Item>
+          <Menu.Item
+            name="Guarantor"
+            active={stateToggle.guarantorToggle === false}
+            onClick={() => toggleHandler('guarantorToggle')}
+          >
+            <b>3. Guarantor</b>
+          </Menu.Item>
+          <Menu.Item
+            name="Farm"
+            active={stateToggle.farmInfoToggle === false}
+            onClick={() => toggleHandler('farmInfoToggle')}
+          >
+            <b>4. Farm</b>
+          </Menu.Item>
+        </Menu>
+      </Segment>
+      <Form
+        onSubmit={formHandler}
+        style={{
+          marginBottom: `${window.innerHeight / 2}px`
+        }}
+      >
+        <Segment
+          style={{ width: '100%', padding: '2.5rem 1rem' }}
+          hidden={stateToggle.personalInfoToggle}
+        >
+          <div
+            style={{ maxWidth: '500px', margin: '0 auto', textAlign: 'left' }}
+          >
+            {personalInfoInputs}
+          </div>
+          <div
+            style={{
+              maxWidth: '500px',
+              margin: '0 auto',
+              textAlign: 'center',
+              marginTop: '2rem'
+            }}
+          >
+            <Button
+              type="button"
+              onClick={() => toggleHandler('familyInfoToggle')}
+              size="large"
+              content="Next"
+              icon="right arrow"
+              labelPosition="right"
+            />
+          </div>
+        </Segment>
 
-        <form action="" onSubmit={(e) => {
-          e.preventDefault();
-        }} style={{ padding: '2rem' }}>
-          <fieldset>
-            <DivToggle onClick={toggleHandler.bind(this, 'personalInfoToggle')}>
-              <h2>Personal Information</h2>
-              <i className="fas fa-angle-double-down fa-2x" />
-            </DivToggle>
-            <div hidden={stateToggle.personalInfoToggle}>{personalInfoInputs}</div>
-          </fieldset>
+        <Segment
+          style={{ width: '100%', padding: '2.5rem 1rem' }}
+          hidden={stateToggle.familyInfoToggle}
+        >
+          <div
+            style={{ maxWidth: '500px', margin: '0 auto', textAlign: 'left' }}
+          >
+            {familyInfoInputs}
+          </div>
+          <div
+            style={{
+              maxWidth: '500px',
+              margin: '0 auto',
+              textAlign: 'center',
+              marginTop: '2rem'
+            }}
+          >
+            <Button
+              type="button"
+              onClick={() => toggleHandler('guarantorToggle')}
+              size="large"
+              content="Next"
+              icon="right arrow"
+              labelPosition="right"
+            />
+          </div>
+        </Segment>
 
-          <hr />
-          <fieldset>
-            <DivToggle onClick={toggleHandler.bind(this, 'familyInfoToggle')}>
-              <h2>Family</h2> <i className="fas fa-angle-double-down fa-2x" />
-            </DivToggle>
-            <div hidden={stateToggle.familyInfoToggle}>{familyInfoInputs}</div>
-          </fieldset>
+        <Segment
+          style={{ width: '100%', padding: '2.5rem 1rem' }}
+          hidden={stateToggle.guarantorToggle}
+        >
+          <div
+            style={{ maxWidth: '500px', margin: '0 auto', textAlign: 'left' }}
+          >
+            {guarantorInputs}
+          </div>
+          <div
+            style={{
+              maxWidth: '500px',
+              margin: '0 auto',
+              textAlign: 'center',
+              marginTop: '2rem'
+            }}
+          >
+            <Button
+              type="button"
+              onClick={() => toggleHandler('farmInfoToggle')}
+              size="large"
+              content="Next"
+              icon="right arrow"
+              labelPosition="right"
+            />
+          </div>
+        </Segment>
 
-          <hr />
-          <fieldset>
-            <DivToggle onClick={toggleHandler.bind(this, 'guarantorToggle')}>
-              <h2>Guarantor</h2> <i className="fas fa-angle-double-down fa-2x" />
-            </DivToggle>
-            <div hidden={stateToggle.guarantorToggle}>{guarantorInputs}</div>
-          </fieldset>
-          <hr />
-
-          <fieldset>
-            <DivToggle onClick={toggleHandler.bind(this, 'farmInfoToggle')}>
-              <h2>Farm Information</h2> <i className="fas fa-angle-double-down fa-2x" />
-            </DivToggle>
-            <div hidden={stateToggle.farmInfoToggle}>{farmInfoInputs}</div>
-          </fieldset>
-        </form>
-          
-        {/* Button is wrapped in a div to get onClick to work */}
-        <div onClick={(e) => formHandler(e)}>
-          <Button displayName="Save" />
-        </div>
-      </section>
+        <Segment
+          style={{ width: '100%', padding: '2.5rem 1rem' }}
+          hidden={stateToggle.farmInfoToggle}
+        >
+          <div style={{ maxWidth: '500px', margin: '0 auto' }}>
+            {farmInfoInputs}
+          </div>
+          <div
+            style={{
+              maxWidth: '500px',
+              margin: '0 auto',
+              textAlign: 'center',
+              marginTop: '2rem'
+            }}
+          >
+            <Button
+              primary
+              type="submit"
+              size="large"
+              content="Submit Changes"
+              icon="check"
+              labelPosition="right"
+            />
+          </div>
+        </Segment>
+      </Form>
     </div>
-  )
-}
+  );
+};
 
 export default withRestrictedAccess(UpdateFarmer);
