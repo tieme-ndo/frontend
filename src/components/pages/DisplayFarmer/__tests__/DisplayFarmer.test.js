358ff4cd1640b6c96305d8de2382e53b5da770d4
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-09-05 15:14:40 +0200

    Tests for tabs functionality

--- src/components/pages/DisplayFarmer/__tests__/DisplayFarmer.test.js
+++ src/components/pages/DisplayFarmer/__tests__/DisplayFarmer.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import DisplayFarmer from './DisplayFarmer';
+import DisplayFarmer from '../DisplayFarmer';
 
 beforeAll(() => {
   // Mocking localStorage
@@ -15,6 +15,8 @@ beforeAll(() => {
   global.localStorage = localStorageMock;
 });
 
+
+
 const farmers = [
   {
     _id: 'aksgjkasjgkasga',
@@ -23,7 +25,12 @@ const farmers = [
       middle_name: 'Smith',
       surname: 'Thing',
       image_url: ''
-    }
+    },
+    familyInfo:{
+      family_size:5,
+      number_of_dependant:3,
+      highest_level_of_dependent:"Tertiary",
+      family_income_per_month:"More than GHC 1,000"}
   }
 ];
 
@@ -43,5 +50,23 @@ it('renders without crashing', () => {
   );
 
   const DisplayFarmerComponent = getByTestId('farmer-display-test');
+  expect(DisplayFarmerComponent).toBeInTheDocument();
+
+  // It renders the personal tab by default
+  const PersonalTabComponent = getByTestId('personal-tab-render-test');
+  expect(PersonalTabComponent).toBeInTheDocument();
+});
+
+it('renders the respective tab when clicking tab link', () => {
+  localStorage.setItem('tokenTiemeNdo', 'akgjsakgjaslgjslgkjaslgjalkgja');
+
+  const { getByTestId } = render(
+    <Router>
+      <DisplayFarmer getFarmer={getFarmer} farmers={farmers} match={match} />
+    </Router>
+  );
+
+  fireEvent.click(getByTestId('family-tab-click-test'))
+  const DisplayFarmerComponent = getByTestId('family-tab-render-test');
   expect(DisplayFarmerComponent).toBeInTheDocument();
 });
