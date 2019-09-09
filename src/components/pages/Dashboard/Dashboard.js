f512f57ae2e315835eefd0fa3614d348e7593be2
Shola Ayeni <ayenisholah@yahoo.com>
2019-09-09 11:00:40 +0100

    search component

--- src/components/pages/Dashboard/Dashboard.js
+++ src/components/pages/Dashboard/Dashboard.js
@@ -3,13 +3,24 @@ import DashboardHeader from './DashboardHeader';
 import tableColumLabels from './tableColumLabels';
 import DashboardTable from '../../common/Table/Table';
 import LoadingIndicator from './LoadingIndicator';
-import { Header, Button } from 'semantic-ui-react';
+import { Header, Button, Grid, Search } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import withRestrictedAccess from '../../hoc/withRestrictedAccess';
 import PropTypes from 'prop-types';
 
+// Set display filter to an empty string
+// Filter names based on key press
+
+
+
+
+
 const Dashboard = ({ farmers, history }) => {
+
   const [data, setData] = React.useState([]);
+
+  const [search, setSearch] = React.useState('shola');
+
 
   const Title = <Header as="h1">All Farmers</Header>;
 
@@ -22,7 +33,6 @@ const Dashboard = ({ farmers, history }) => {
       setData([]);
     }
   }, [farmers]);
-
   return (
     <div data-testid="dashboard-render-test">
       <DashboardHeader
@@ -34,7 +44,17 @@ const Dashboard = ({ farmers, history }) => {
             </Button>
           </Link>
         }
+        farmers={farmers}
       />
+
+      <Grid style={{ marginBottom: '10px' }}>
+        <Grid.Column width={6}>
+          <Search 
+            value={search}
+            onSearchChange={event => setSearch(event.target.value)}
+          />
+        </Grid.Column>
+      </Grid>
 
       {data.length ? (
         <DashboardTable
@@ -43,8 +63,8 @@ const Dashboard = ({ farmers, history }) => {
           data={data}
         />
       ) : (
-        <LoadingIndicator />
-      )}
+          <LoadingIndicator />
+        )}
     </div>
   );
 };
