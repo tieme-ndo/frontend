94db4be322547e30f1548406b0485884a15aa56b
Shola Ayeni <ayenisholah@yahoo.com>
2019-09-11 07:46:34 +0100

    display and hide notification on icon click

--- src/components/common/Notification/notification.js
+++ src/components/common/Notification/notification.js
@@ -1,23 +1,42 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const Container = styled.div`
-  color: palevioletred;
-  z-index: 999;
-  transition: top 0.5s ease;
-`
+
 
 
 const Notification = ({ user }) => {
+
+  const [top, setTop] = React.useState(-100)
+
+  const NotificationDisplay = styled.div`
+  background-color: palevioletred;
+  color: black;
+  padding: 16px;
+  position: absolute;
+  top: ${top}px;
+  right: 16px;
+  z-index: 999;
+  transition: top 0.5s ease;
+`
+  // const onShow = () => {
+
+  // }
+
+  React.useEffect(function showNotification() {
+    setTop(16)
+  }, [])
   return (
-    <Container>
-      {
-        user && user.isAdmin ? (
-          <i className="fa fa-bell fa-2x" style={{ padding: '12px', color: 'palevioletred' }}></i>
-        ) : null
-      }
-    </Container>
+    <>
+      <button onClick={setTop}>
+        {
+          user && user.isAdmin ? (
+            <i className="fa fa-bell fa-2x" style={{ padding: '12px', color: 'palevioletred' }}></i>
+          ) : null
+        }
+      </button>
+      <NotificationDisplay>Hellur!</NotificationDisplay>
+    </>
   )
 }
 
