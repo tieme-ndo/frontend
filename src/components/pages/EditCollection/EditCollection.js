62a9d018ff6364e41cefdd42de10590fb749f768
Shola Ayeni <ayenisholah@yahoo.com>
2019-09-13 11:15:57 +0100

    resolve merge conflict

--- src/components/pages/EditCollection/EditCollection.js
+++ src/components/pages/EditCollection/EditCollection.js
@@ -1,67 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Grid, Segment, Step } from 'semantic-ui-react';
-
-const EditCollection = props => {
-//   console.log(props);
-  return (
-    <div data-testid="edit-collection-component">
-      <h1>Edit Collection</h1>
-      <Segment>
-        <Grid>
-          <Grid.Column width={5}>
-            <Step.Group vertical>
-              <Step>
-                <Step.Content>
-                  <Step.Description>
-                    <h3>
-                      {' '}
-                      <a href="/">Staff name edit Farmer’s name record</a>
-                    </h3>
-                  </Step.Description>
-                </Step.Content>
-              </Step>
-              <Step>
-                <Step.Content>
-                  <Step.Description>
-                    <h3>
-                      {' '}
-                      <a href="/">Staff name edit Farmer’s name record</a>
-                    </h3>
-                  </Step.Description>
-                </Step.Content>
-              </Step>
-              <Step>
-                <Step.Content>
-                  <Step.Description>
-                    <h3>
-                      {' '}
-                      <a href="/">Staff name edit Farmer’s name record</a>
-                    </h3>
-                  </Step.Description>
-                </Step.Content>
-              </Step>
-            </Step.Group>
-          </Grid.Column>
-          <Grid.Column width={11}>
-            <Segment>
-              Join Solcioty Fitness in our fight to raise money for numerous
-              organizations and charities in our community!We are partnering up
-              with local health and wellness businesses to raise money for the
-              charity of their preference. We will compete in a series of field
-              day activities that are all fitness based in order to create a fun
-              and competitive environment with the reward of giving back to our
-              community. Each team will pick a charity that hits home for their
-              business, and we will be competing in these different events to
-              win the jackpot for the individual charities.$20 of your admission
-              fee will go towards the jackpot while the other $15 will go
-              towards your individual team's charity, so that ev…
-            </Segment>
-          </Grid.Column>
-        </Grid>
-      </Segment>
-    </div>
-=======
 import React, { useState } from 'react';
 import { Grid, Segment, Table, TableHeader, Button } from 'semantic-ui-react';
 import styled from 'styled-components';
@@ -167,7 +103,6 @@ const EditCollection = props => {
         Accept
       </Button>
     </Div>
->>>>>>> a781a048da50c198711494598c851e1397cff43f
   );
 };
 
