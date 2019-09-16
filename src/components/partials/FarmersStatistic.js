4c6fd55fe5cce46c65e7ede8556aa1f34c422473
abidex4yemi <abidex4yemi@gmail.com>
2019-09-16 16:21:01 +0100

    Complete get farmers statistic feature

--- src/components/partials/FarmersStatistic.js
+++ src/components/partials/FarmersStatistic.js
@@ -4,55 +4,46 @@ import { Statistic, Container, Segment } from 'semantic-ui-react';
 const FarmersStatistic = props => {
   return (
     <Container style={{ marginBottom: '40px' }}>
-      <Segment.Group horizontal style={{ fontWeight: 'normal' }}>
-        <Segment textAlign="center">
+      <Segment.Group horizontal>
+        <Segment textAlign="center" style={{ padding: '14px 0 0 0' }}>
           <Statistic
             label="Total Farmers"
             value={`${props.totalNumOfFarmers}`}
             color="teal"
-            size="small"
+            size="tiny"
           />
         </Segment>
-        <Segment textAlign="center">
-          <Statistic color="blue" size="tiny">
+
+        <Segment textAlign="center" style={{ padding: '14px 0 0 0' }}>
+          <Statistic color="green" size="tiny">
             <Statistic.Value>{props.totalNumOfFemaleFarmers}</Statistic.Value>
-            <Statistic.Label style={{ fontWeight: 'normal' }}>
-              Female
-            </Statistic.Label>
+            <Statistic.Label>Female</Statistic.Label>
           </Statistic>
 
-          <Statistic color="blue" size="tiny">
+          <Statistic color="green" size="tiny">
             <Statistic.Value>{props.totalNumOfMaleFarmers}</Statistic.Value>
-            <Statistic.Label style={{ fontWeight: 'normal' }}>
-              Male
-            </Statistic.Label>
+            <Statistic.Label>Male</Statistic.Label>
           </Statistic>
 
-          <Statistic color="blue" size="tiny">
+          <Statistic color="green" size="tiny">
             <Statistic.Value>0</Statistic.Value>
-            <Statistic.Label style={{ fontWeight: 'normal' }}>
-              Other
-            </Statistic.Label>
+            <Statistic.Label>Other</Statistic.Label>
           </Statistic>
         </Segment>
 
-        <Segment textAlign="center">
-          <Statistic color="blue" size="tiny">
+        <Segment textAlign="center" style={{ padding: '14px 0 0 0' }}>
+          <Statistic color="green" size="tiny">
             <Statistic.Value>
               {props.farmersAgeGreaterThanOrEqualThirtyFive}
             </Statistic.Value>
-            <Statistic.Label style={{ fontWeight: 'normal' }}>
-              Above 35 Years Old
-            </Statistic.Label>
+            <Statistic.Label>Above 35 Years Old</Statistic.Label>
           </Statistic>
 
-          <Statistic color="blue" size="tiny">
+          <Statistic color="green" size="tiny">
             <Statistic.Value>
               {props.farmersAgeLesserThanThirtyFive}
             </Statistic.Value>
-            <Statistic.Label style={{ fontWeight: 'normal' }}>
-              Below 35 Years Old
-            </Statistic.Label>
+            <Statistic.Label>Below 35 Years Old</Statistic.Label>
           </Statistic>
         </Segment>
       </Segment.Group>
