a402c1991175c8477e5cf2d000a42dee26460c5c
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-09-17 15:48:20 +0100

    Use Custom Statistics Styled Components

--- src/components/partials/FarmersStatistic.js
+++ src/components/partials/FarmersStatistic.js
@@ -1,51 +1,48 @@
 import React from 'react';
-import { Statistic, Container, Segment } from 'semantic-ui-react';
+import { Container, Segment } from 'semantic-ui-react';
+import {
+  MajorStatistic,
+  MajorStatContainer,
+  MinorStatistic,
+  MinorStatContainer
+} from './StyledStatistics';
 
 const FarmersStatistic = props => {
   return (
     <Container style={{ marginBottom: '40px' }}>
-      <Segment.Group horizontal>
-        <Segment textAlign="center" style={{ padding: '14px 0 0 0' }}>
-          <Statistic
+      <Segment.Group horizontal style={{ padding: '25px 0' }}>
+        <MajorStatContainer>
+          <MajorStatistic
             label="Total Farmers"
             value={`${props.totalNumOfFarmers}`}
-            color="teal"
-            size="tiny"
           />
-        </Segment>
+        </MajorStatContainer>
 
-        <Segment textAlign="center" style={{ padding: '14px 0 0 0' }}>
-          <Statistic color="green" size="tiny">
-            <Statistic.Value>{props.totalNumOfFemaleFarmers}</Statistic.Value>
-            <Statistic.Label>Female</Statistic.Label>
-          </Statistic>
+        <MinorStatContainer>
+          <MinorStatistic
+            label="Female"
+            value={props.totalNumOfFemaleFarmers}
+          />
+          <MinorStatistic
+            label="Male"
+            value={props.totalNumOfMaleFarmers}
+          />
+          <MinorStatistic
+            label="Other"
+            value={props.totalNumOfMaleFarmers}
+          />
+        </MinorStatContainer>
 
-          <Statistic color="green" size="tiny">
-            <Statistic.Value>{props.totalNumOfMaleFarmers}</Statistic.Value>
-            <Statistic.Label>Male</Statistic.Label>
-          </Statistic>
-
-          <Statistic color="green" size="tiny">
-            <Statistic.Value>0</Statistic.Value>
-            <Statistic.Label>Other</Statistic.Label>
-          </Statistic>
-        </Segment>
-
-        <Segment textAlign="center" style={{ padding: '14px 0 0 0' }}>
-          <Statistic color="green" size="tiny">
-            <Statistic.Value>
-              {props.farmersAgeGreaterThanOrEqualThirtyFive}
-            </Statistic.Value>
-            <Statistic.Label>Above 35 Years Old</Statistic.Label>
-          </Statistic>
-
-          <Statistic color="green" size="tiny">
-            <Statistic.Value>
-              {props.farmersAgeLesserThanThirtyFive}
-            </Statistic.Value>
-            <Statistic.Label>Below 35 Years Old</Statistic.Label>
-          </Statistic>
-        </Segment>
+        <MinorStatContainer>
+          <MinorStatistic
+            label="Above 35 Years Old"
+            value={props.farmersAgeGreaterThanOrEqualThirtyFive}
+          />
+          <MinorStatistic
+            label="Below 35 Years Old"
+            value={props.totalNumOfMaleFarmers}
+          />
+          </MinorStatContainer>
       </Segment.Group>
     </Container>
   );
