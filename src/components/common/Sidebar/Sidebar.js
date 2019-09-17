e3015505ac8b0ab61ec31b3bde67eb66dc2fac58
Pav0l <pavol.karas@gmail.com>
2019-09-17 12:57:52 +0200

    Fix typo

--- src/components/common/Sidebar/Sidebar.js
+++ src/components/common/Sidebar/Sidebar.js
@@ -7,6 +7,46 @@ import moment from 'moment';
 
 import { Menu, Sidebar, Segment } from 'semantic-ui-react';
 
+const SidebarComponent = ({ visible, edits, closeSideBar }) => {
+  return (
+    <Span data-testid="sidebar-component">
+      {' '}
+      <Sidebar
+        as={Menu}
+        animation="overlay"
+        direction="right"
+        icon="labeled"
+        dimmed={'dimmed'}
+        vertical
+        visible={visible}
+        width="wide"
+        style={{
+          top: '57px',
+          maxHeight: '400px'
+        }}
+      >
+        <Div>
+          {edits && edits.length ? (
+            edits.map(edit => (
+              <Link to={`/edit-collection/${edit._id}`} key={edit._id}>
+                <Segment vertical className="cards" onClick={closeSideBar}>
+                  <p>
+                    <b>{edit.change_requested_by}</b> updated{' '}
+                    <b>{edit.farmer_name}'s record</b> on{' '}
+                    {moment(edit.date).format('LLLL')}
+                  </p>
+                </Segment>
+              </Link>
+            ))
+          ) : (
+            <p>No change requests have been made</p>
+          )}
+        </Div>
+      </Sidebar>
+    </Span>
+  );
+};
+
 const Span = styled.span`
   cursor: pointer;
   padding-top: 10px;
@@ -41,45 +81,5 @@ const Div = styled.div`
     }
   }
 `;
-
-const SidebarComponent = ({ visible, edits, closeSideBar }) => {
-  return (
-    <Span data-testid="sidebar-component">
-      {' '}
-      <Sidebar
-        as={Menu}
-        animation="overlay"
-        direction="right"
-        icon="labeled"
-        dimmed={'dimmed'}
-        vertical
-        visible={visible}
-        width="wide"
-        style={{
-          top: '57px',
-          maxHeight: '400px'
-        }}
-      >
-        <Div>
-          {edits && edits.length ? (
-            edits.map(edit => (
-              <Link to={`/edit-collection/${edit._id}`} key={edit._id}>
-                <Segment vertical className="cards" onClick={closeSideBar}>
-                  <p>
-                    <b>{edit.change_requested_by}</b> updated{' '}
-                    <b>{edit.farmer_name}'s record</b> on{' '}
-                    {moment(edit.date).format('LLLL')}
-                  </p>
-                </Segment>
-              </Link>
-            ))
-          ) : (
-            <p>No change requests has been made</p>
-          )}
-        </Div>
-      </Sidebar>
-    </Span>
-  );
-};
 
 export default SidebarComponent;
