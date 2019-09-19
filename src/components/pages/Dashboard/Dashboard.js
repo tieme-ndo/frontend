e774988df39ee15fa51ea38e79a7a7b240a8d4d2
David Quiñones <daquinons@users.noreply.github.com>
2019-09-19 10:53:36 +0200

    Improve loading of statistics

--- src/components/pages/Dashboard/Dashboard.js
+++ src/components/pages/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Dimmer, Loader, Segment, Header, Button } from 'semantic-ui-react';
+import { Header, Button } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
@@ -13,10 +13,7 @@ import { getfarmerStatisticsHandler } from '../../../utils/handlers/farmerHandle
 
 const Dashboard = ({ farmers, history }) => {
   const [data, setData] = useState([]);
-  const [farmersStatistic, setFarmersStatistic] = useState({});
-  const [farmersStatisticsLoading, setFarmersStatisticsLoading] = useState(
-    true
-  );
+  const [farmersStatistic, setFarmersStatistic] = useState(undefined);
 
   const Title = <Header as="h1">All Farmers</Header>;
 
@@ -25,48 +22,18 @@ const Dashboard = ({ farmers, history }) => {
   useEffect(() => {
     if (farmers) {
       setData(farmers);
+      getfarmerStatisticsHandler()
+        .then(statistics => {
+          setFarmersStatistic(statistics);
+        })
+        .catch(error => {
+          setFarmersStatistic({});
+          toast.error(error.message);
+        });
     } else {
       setData([]);
     }
   }, [farmers]);
-
-  useEffect(() => {
-    setFarmersStatisticsLoading(true);
-    getfarmerStatisticsHandler()
-      .then(res => {
-        setFarmersStatistic(res);
-        setFarmersStatisticsLoading(false);
-      })
-      .catch(error => {
-        toast.error(error.message);
-        setFarmersStatisticsLoading(false);
-      });
-    //   .finally(() => setFarmersStatisticsLoading(false));
-  }, []);
-
-  const renderFarmersStatistic = () => {
-    if (farmersStatisticsLoading) {
-      return (
-        <Segment
-          style={{ marginBottom: '50px', boxShadow: 'none', border: '0' }}
-        >
-          <Dimmer active inverted>
-            <Loader inverted>Loading farmers statistic</Loader>
-          </Dimmer>
-        </Segment>
-      );
-    }
-
-    if (Object.keys(farmersStatistic).length === 0) {
-      return (
-        <Segment secondary style={{ textAlign: 'center' }}>
-          Failed to retreive farmer statistics
-        </Segment>
-      );
-    }
-
-    return <FarmersStatistic {...farmersStatistic} />;
-  };
 
   return (
     <div data-testid="dashboard-render-test">
@@ -81,7 +48,7 @@ const Dashboard = ({ farmers, history }) => {
         }
       />
 
-      {renderFarmersStatistic()}
+      <FarmersStatistic farmersStatistic={farmersStatistic} />
 
       {data.length ? (
         <DashboardTable history={history} columns={columns} data={data} />
