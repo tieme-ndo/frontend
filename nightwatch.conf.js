0278c5fffcef5226c111d52c269963c4ea472004
David Quiñones <daquinons@users.noreply.github.com>
2019-09-25 09:51:56 +0200

    Update config file

---
+++ nightwatch.conf.js
@@ -0,0 +1,17 @@
+const chrome = require('chromedriver');
+
+module.exports = {
+  src_folders: ['tests'],
+  webdriver: {
+    start_process: true,
+    server_path: chrome.path,
+    port: 9515
+  },
+  test_settings: {
+    default: {
+      desiredCapabilities: {
+        browserName: 'chrome'
+      }
+    }
+  }
+};
